2719538bac2664795afaa235b8dd9d75dd664b42
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-03-08 16:56:13 +0100

    Time: 28 ms (32.22%), Space: 71.7 MB (67.37%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,28 +1,35 @@
 class Solution {
-    public int[][] updateMatrix(int[][] mat){
-     Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 0)
-                    queue.add(new int[] { i, j });
-                else
-                    mat[i][j] = -1;
-
+    // look for dfs as well!!!!!
+    public int[][] updateMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                }
+                else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
-        int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
         while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            for (int[] dir : directions) {
-                int a = poll[0] + dir[0];
-                int b = poll[1] + dir[1];
-                if (a < 0 || a >= mat.length || b < 0 || b >= mat[0].length || mat[a][b] != -1)
-                    continue;
-                mat[a][b] = mat[poll[0]][poll[1]] + 1;
-                queue.add(new int[] { a, b });
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || r >= m || c < 0 || c >= n || 
+                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
             }
-
         }
-        return mat;
-}
+        
+        return matrix;
+    }
 }
