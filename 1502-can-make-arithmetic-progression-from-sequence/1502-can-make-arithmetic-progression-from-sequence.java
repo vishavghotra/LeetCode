07ccbe34fa6a1128c563c5401863e4823fbb1440
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-03-09 12:03:19 +0100

    Time: 2 ms (77.73%), Space: 42.2 MB (50.86%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,10 +1,32 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for(int i  = 2; i< arr.length; i++){
-            if(arr[i] - arr[i-1] != diff)
-                return false;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int num: arr){
+            max = Math.max(num,max);
+            min = Math.min(num,min);
+        }
+        int gap = (max - min)/(arr.length - 1);
+        if(max - min != gap * (arr.length - 1)) return false;
+        //Case 1: {1,1,1,1} or {0,0,0,0}
+        if(gap == 0 && min == max) return true;
+        //swap each num to according place 
+        //min + 0*gap, min + 1*gap, min + 2*gap,..., min + (arr.length-1)*gap
+        for(int i = 0;i<arr.length;){
+        //Case 2: {1,2,5,7} meet with arr[i] which can not be put anywhere
+            if((arr[i] - min) % gap != 0) return false;
+            int index = (arr[i] - min)/gap;
+        //Case 3: {1,2,3,4} -> {1,2,3,4} i++ when arr[i] is at the right place
+            if(index == i){
+                i++;
+            }else{
+        //Case 4: {1,2,2,4} meet with duplicate
+                if(arr[index] == arr[i]) return false;
+        //Case 5: {2,1,3,4} -> {1,2,3,4} after swap 2 and 1
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
         }
         return true;
     }
