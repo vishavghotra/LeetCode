6983f6afa147df2f297163db6c1022cd7bf568c1
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-03-12 23:24:33 +0100

    Time: 4 ms (30.25%), Space: 76.4 MB (76.14%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -3,26 +3,14 @@ class Solution {
         // see first answer kedane's algo
         // different from part 2 because 2 requires full profit, not max difference from any two days
         
-       /* int max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         int intermax  = 0;
         for(int i = 1; i< prices.length; i++) {
             intermax = Math.max(intermax + prices[i] - prices[i-1], 0) ;
             max= Math.max(max, intermax);
             
         }
-        return (max <= 0)?0:max; */
-        
-        int min =prices[0];
-        int max =0;
-        for(int i = 1; i< prices.length; i++) {
-            if(prices[i] < min) {
-                min  = prices[i];
-                
-            }
-            else max= Math.max(prices[i] - min, max);
-            
-        }
-        return max;
+        return (max <= 0)?0:max;
      
     }
 }
