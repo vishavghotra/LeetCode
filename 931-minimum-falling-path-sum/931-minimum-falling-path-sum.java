a5daee112fd6126bb3e4bad230facdec865fe43e
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-03-18 08:41:53 +0100

    Time: 135 ms (5.11%), Space: 54.6 MB (5.01%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        dp[matrix.length-1] = matrix[matrix.length -1];
+        for(int i = 0; i < matrix.length; i++) {
+            dp[matrix.length -1 ][i] = matrix[matrix.length - 1][i];
+        }
+        for(int i = matrix.length -2; i >=0 ; i-- ) {
+for(int j = 0; j< matrix[0].length; j++) {
+    if(j == 0) {
+        dp[i][j] = Math.min(matrix[i][j] + dp[i+1][j], matrix[i][j] + dp[i+1][j+1]);
+        
+    }
+    else if (j == matrix[0].length -1) {
+        dp[i][j] = Math.min(matrix[i][j] + dp[i+1][j], matrix[i][j] + dp[i+1][j-1]);
+        
+    }
+    else {
+        dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j],Math.min(dp[i+1][j-1], dp[i+1][j+1]));
+    }
+    System.out.println(dp[i][j]);
+    
+}
+        
+        
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i< matrix.length; i++) {
+            if(min > dp[0][i]) min = dp[0][i];
+            
+        }
+        return min;
+    }
+}
