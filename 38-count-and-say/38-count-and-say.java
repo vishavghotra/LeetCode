1e33acc92be38f389be8a903b5eef116cac338c5
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-03-19 15:03:38 +0100

    Time: 2 ms (93.84%), Space: 42.6 MB (42.93%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,27 +1,26 @@
 class Solution {
      public String countAndSay(int n) {
-    String result = "1";
-    for (int i = 2; i <= n; i++) {
-      result = build(result);
+        String result = "1";
+        for(int i = 2; i <= n; i++){
+            result = next(result);
+        }
+        return result;
     }
-    return result;
-
-  }
-
-  public String build(String result) {
-    int index = 0;
-    String build = "";
-    while (index < result.length()) {
-      char c = result.charAt(index);
-      int count = 0;
-      while (index < result.length() && result.charAt(index) == c) {
-        index++;
-        count++;
-      }
-      build += count + "" + c;
-
+    private String next(String input) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = input.toCharArray();
+        char curr = arr[0];
+        int count = 1;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] == curr){
+                count++;
+            } else {
+                sb.append(count).append(curr);
+                curr = arr[i];
+                count = 1;
+            }
+        }
+        sb.append(count).append(curr);
+        return sb.toString();
     }
-    return build;
-
-  }
 }
