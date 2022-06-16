23698d6bb5cf799155717ded26ec75535810ba9b
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-06-17 03:06:06 +0530

    Time: 365 ms (28.05%), Space: 99.1 MB (13.62%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,23 +1,23 @@
 class Solution {
-    // use other approach with constant space
-  public String longestPalindrome(String s) {
-  int n = s.length();
-
-    
-  boolean[][] dp = new boolean[n][n];
-    int index1 = 0; int index2 = 0;
-  for (int i = n - 1; i >= 0; i--) {
+    public String longestPalindrome(String s) {
+         boolean[][] dp = new boolean[s.length() + 1][s.length() + 1];
+    int max = 1;
+    String result = s.substring(s.length() - 1);
+    for (int i = 0; i < s.length(); i++)
       dp[i][i] = true;
-    for (int j = i + 1; j < n; j++) {
-      dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
-            
-      if (dp[i][j] && (j - i + 1 > index2 - index1 + 1)) {
-        index1 = i;
-          index2 = j;
+    for (int i = s.length() - 2; i >= 0; i--) {
+      for (int j = i + 1; j < s.length(); j++) {
+        if (s.charAt(i) == s.charAt(j) && (j == i + 1 || dp[i + 1][j - 1])) {
+          dp[i][j] = true;
+          if (j - i + 1 > max) {
+            result = s.substring(i, j + 1);
+            max = j - i + 1;
+          }
+        }
+
       }
+
     }
-  }
-    
-  return s.substring(index1, index2+1);
-}
+    return result;
+    }
 }
