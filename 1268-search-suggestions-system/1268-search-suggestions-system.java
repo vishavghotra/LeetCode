449faf7682ce218b8350c43e931b2c0a6b5bd1a9
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-06-23 13:32:34 +0530

    Time: 52 ms (53.46%), Space: 72.8 MB (39.13%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,56 @@
+class Solution {
+     class TrieNode {
+    Map<Character, TrieNode> map;
+    List<String> words;
+
+    public TrieNode() {
+      map = new HashMap<>();
+      words = new ArrayList<>();
+
+    }
+  }
+
+  TrieNode root = new TrieNode();
+
+  public void insert(String word) {
+    TrieNode curr = root;
+    for (int i = 0; i < word.length(); i++) {
+      if (curr.map.get(word.charAt(i)) == null) {
+        curr.map.put(word.charAt(i), new TrieNode());
+      }
+      curr = curr.map.get(word.charAt(i));
+      curr.words.add(word);
+
+    }
+
+  }
+
+  public List<List<String>> suggestedProducts(String[] products, String searchword) {
+    Arrays.sort(products);
+    for (String str : products) {
+      insert(str);
+    }
+    List<List<String>> result = new ArrayList<>();
+    TrieNode curr = root;
+    for (int i = 0; i < searchword.length(); i++) {
+
+      if (curr == null)
+        result.add(new ArrayList<>());
+      else if (curr.map.get(searchword.charAt(i)) == null) {
+
+        result.add(new ArrayList<>());
+        curr = null;
+      } else {
+        curr = curr.map.get(searchword.charAt(i));
+        List<String> adds = new ArrayList<>();
+        for (int j = 0; j < curr.words.size(); j++) {
+          if (j == 3)
+            break;
+          adds.add(curr.words.get(j));
+        }
+        result.add(adds);
+      }
+    }
+    return result;
+  }
+}
