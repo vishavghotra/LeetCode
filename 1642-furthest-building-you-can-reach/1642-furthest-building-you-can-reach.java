e8df45a3db23784853c16ac578bab78aef2da9c3
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-06-27 13:44:00 +0530

    Time: 29 ms (65.80%), Space: 70.2 MB (75.59%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -22,4 +22,26 @@ class Solution {
     }
     return result;
     }
+    
+    /*
+    
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+	for (int i=0; i<heights.length-1; i++) {
+		if (heights[i] >= heights[i+1]) continue;
+		bricks -= heights[i+1] - heights[i];
+		pq.add(heights[i+1] - heights[i]);
+
+		if (bricks < 0) {
+			bricks += pq.poll();
+			if (ladders > 0) ladders--;
+			else return i;
+		}
+	}
+
+	return heights.length - 1;
+    
+    
+    
+    */
 }
