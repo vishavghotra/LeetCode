4a53d535974e27ffddc62d0bc27e183783170819
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-06-27 14:26:47 +0530

    Time: 16 ms (16.82%), Space: 44.3 MB (69.97%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-     public int findKthLargest(int[] nums, int k) {
+   /*  public int findKthLargest(int[] nums, int k) {
     return partition(0, nums.length - 1, nums, nums.length - k);
   }
 
@@ -28,7 +28,34 @@ class Solution {
     nums[i] = nums[j];
     nums[j] = ex;
 
-  }
+  }  */
+    
+     public int findKthLargest(int[] nums, int k) {
+        int start = 0, end = nums.length - 1, index = nums.length - k;
+        while (start < end) {
+            int pivot = partion(nums, start, end);
+            if (pivot < index) start = pivot + 1; 
+            else if (pivot > index) end = pivot - 1;
+            else return nums[pivot];
+        }
+        return nums[start];
+    }
+    
+    private int partion(int[] nums, int start, int end) {
+        int pivot = start, temp;
+        while (start <= end) {
+            while (start <= end && nums[start] <= nums[pivot]) start++;
+            while (start <= end && nums[end] > nums[pivot]) end--;
+            if (start > end) break;
+            temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+        temp = nums[end];
+        nums[end] = nums[pivot];
+        nums[pivot] = temp;
+        return end;
+    }
 
 
 }
