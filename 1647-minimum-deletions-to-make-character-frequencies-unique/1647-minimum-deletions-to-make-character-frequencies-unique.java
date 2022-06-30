62ade53f93ba68995871f31d78879649205f3840
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-07-01 05:23:25 +0530

    Time: 28 ms (28.10%), Space: 72.8 MB (5.49%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletions(String s) {
+        int[] cnt = new int[26];
+    int[] fr = new int[100001];
+    int result = 0;
+    for (char c : s.toCharArray())
+      cnt[c - 'a']++;
+    for (int i = 0; i < 26; i++) {
+      while (cnt[i] > 0 && fr[cnt[i]] > 0) {
+        --cnt[i];
+        ++result;
+      }
+      fr[cnt[i]]++;
+    }
+    return result;
+    }
+}
