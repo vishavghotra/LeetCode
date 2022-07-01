c8d8a0831880fe9c4ee56bffae3e632b2d0f4a06
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-07-01 13:29:00 +0530

    Time: 5 ms (67.09%), Space: 46.5 MB (6.82%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -2,6 +2,10 @@ class Solution {
     public int minMoves2(int[] nums) {
         //initution: 1,2,4,6,8,10 :: 
         //convergence: required number between 1 and 10 -> required number between 2 and 8 -> between 4, 6 -> optimal 5   (like recursion)
+        // for odd: 1,2,3,4,7,10,11 same until 3, 7 -> then optimal is 5 but we have median = 4.
+        //if we move all 3, 7, 4 to 5, total = 4 steps
+        // if we move only 3, 7 to 4 total = 4 steps
+        // REASON : steps(3,5) = steps(3,4) + steps(4,5). One elements steps reduces in (2) which is added to median in (1)
 	Arrays.sort(nums);
 	int operations = 0, mid = nums[nums.length/2];   //
 	for (int num: nums) operations += Math.abs(mid - num);
