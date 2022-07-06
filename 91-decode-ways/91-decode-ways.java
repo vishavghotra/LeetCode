bc4b7cba64647e8afcd9e5c6b4bafb848179da64
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-07-06 22:16:11 +0530

    Time: 3 ms (41.30%), Space: 42.3 MB (53.88%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -10,7 +10,7 @@ class Solution {
         for (int i = 2; i <= n; i++) {
             int first = Integer.valueOf(s.substring(i - 1, i));
             int second = Integer.valueOf(s.substring(i - 2, i));
-            if (first >= 1 && first <= 9) {
+            if (first >= 1) {
                dp[i] += dp[i-1];  
             }
             if (second >= 10 && second <= 26) {
