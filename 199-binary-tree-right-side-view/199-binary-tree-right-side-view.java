e7fac019561acc844957cea7cf1b0a5410cf40ce
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-07-12 03:14:25 +0530

    Time: 1 ms (95.07%), Space: 43.2 MB (9.96%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -14,28 +14,22 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-         if (root == null)
-      return new ArrayList<>();
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    List<Integer> result = new LinkedList<>();
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      for (int i = 0; i < size; i++) {
-        TreeNode pulled = queue.poll();
-
-        if (i == size - 1) {
-          result.add(pulled.val);
-
-        }
-        if (pulled.left != null)
-          queue.add(pulled.left);
-        if (pulled.right != null)
-          queue.add(pulled.right);
-      }
-
+     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
     }
-    return result;
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+        
     }
 }
