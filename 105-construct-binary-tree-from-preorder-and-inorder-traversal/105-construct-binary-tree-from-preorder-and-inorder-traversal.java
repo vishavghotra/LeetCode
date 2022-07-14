40ccdb9ead5d66622279b93a4401abb656d0bc20
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-07-14 22:56:52 +0530

    Time: 6 ms (26.24%), Space: 44.8 MB (28.97%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,29 +15,22 @@
  */
 class Solution {
   public TreeNode buildTree(int[] preorder, int[] inorder) {
-    if (preorder.length == 1) {
-      return new TreeNode(preorder[0]);
-    } else if (preorder.length == 0) {
-      return null;
-    } else {
-      return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
-    }
+    return helper(0, 0, inorder.length - 1, preorder, inorder);
+}
 
-  }
-
-  public TreeNode build(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
-      if(preStart > preEnd || inStart > inEnd)
-          return null;
-    int i = 0;
-    for (i = inStart; i <= inEnd; i++) {
-      if (inorder[i] == preorder[preStart]) {
-        break;
-      }
+public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+    if (preStart > preorder.length - 1 || inStart > inEnd) {
+        return null;
     }
-    int leftLength = i - inStart;
-    TreeNode result = new TreeNode(inorder[i]);
-    result.left = build(preorder, inorder, preStart + 1, preStart + leftLength, inStart, i - 1);
-    result.right = build(preorder, inorder, preStart + leftLength + 1, preEnd, i + 1, inEnd);
-    return result;
-  }
+    TreeNode root = new TreeNode(preorder[preStart]);
+    int inIndex = 0; // Index of current root in inorder
+    for (int i = inStart; i <= inEnd; i++) {
+        if (inorder[i] == root.val) {
+            inIndex = i;
+        }
+    }
+    root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+    root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+    return root;
+}
 }
