77ca5989603c942424b3387fc5586de729d98d35
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-08-23 23:21:53 +0200

    Time: 16 ms (38.07%), Space: 55.2 MB (62.15%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,24 +1,25 @@
 class Solution {
-   public int[][] merge(int[][] intervals) {
-		List<int[]> res = new ArrayList<>();
-        if(intervals.length == 0 || intervals == null) return res.toArray(new int[0][]);
-        
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        
-        for(int[] i : intervals) {
-            if(i[0] <= end) {
-                end = Math.max(end, i[1]);
-            }
-            else {
-                res.add(new int[]{start, end});
-                start = i[0];
-                end = i[1];
-            }
-        }
-        res.add(new int[]{start, end});
-       return res.toArray(new int[0][]);
-   }
+    public int[][] merge(int[][] intervals) {
+         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+    List<int[]> map = new ArrayList<>();
+    for (int i = 1; i < intervals.length; i++) {
+      if (end >= intervals[i][0])
+        end = Math.max(intervals[i][1], end);
+      else {
+        map.add(new int[] { start, end });
+        start = intervals[i][0];
+        end = intervals[i][1];
+
+      }
+    }
+    map.add(new int[] { start, end });
+    int[][] result = new int[map.size()][2];
+    for (int i = 0; i < map.size(); i++) {
+      result[i] = map.get(i);
+
+    }
+    return result;
+    }
 }
