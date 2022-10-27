456c4f0ff2d1ed2219d8afbe280dc4ec58fd0194
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-10-27 20:47:22 +0200

    Time: 67 ms (52.12%), Space: 43.5 MB (44.24%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,4 +1,32 @@
 class Solution {
+    
+    /*
+       int ret = 0;
+    vector<vector<int>> walked;
+    int s;
+    void func(vector<vector<int>>& A, vector<vector<int>>& B, int x, int y){
+        if(abs(x) >= s || abs(y) >= s)return;
+        if(walked[s+x][s+y])return;
+        walked[s+x][s+y] = 1;
+        int cnt = 0;
+        for(int i=0; i<s; i++){
+            for(int j=0; j<s; j++){
+                if(i+x >= s || i+x < 0 || j+y >= s || j+y < 0)continue;
+                if(A[i][j] && B[i+x][j+y])cnt++;
+            }
+        }
+        ret = max(ret, cnt);
+        func(A,B,x+1,y);
+        func(A,B,x,y+1);
+        func(A,B,x-1,y);
+        func(A,B,x,y-1);
+    }
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        walked.resize(A.size()*2,vector<int>(A.size()*2,0));
+        s = A.size();
+        func(A,B,0,0);
+        return ret;
+    }  */
    public int largestOverlap(int[][] A, int[][] B) {
         int ans = 0;
         for (int row = -A.length + 1; row < A.length; row++) {
