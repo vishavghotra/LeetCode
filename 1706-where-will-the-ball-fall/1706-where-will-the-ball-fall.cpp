cddf13050a444676210747af621adb6a71d44ebf
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-10-28 19:32:56 +0200

    Time: 65 ms (22.57%), Space: 13.2 MB (51.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -13,13 +13,10 @@ public:
     {
         if (x >= grid.size())
             return y;
-        if (grid[x][y] == 1 && (y == grid[0].size() - 1 || grid[x][y + 1] != 1))
-            return -1;
-        if (grid[x][y] == -1 && (y == 0 || grid[x][y - 1] != -1))
-            return -1;
-        if (grid[x][y] == 1)
-            return helper(grid, x + 1, y + 1);
-        else
+        if (grid[x][y] == 1 && (y < grid[0].size() - 1 && grid[x][y + 1] == 1))
+           return helper(grid, x + 1, y + 1);
+        if (grid[x][y] == -1 && (y > 0 && grid[x][y - 1] == -1))
             return helper(grid, x + 1, y - 1);
+        return -1;
     }
 };
