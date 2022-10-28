c3b664fe5f3c4aceddc974c9dae27670451c5bbd
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-10-28 19:24:26 +0200

    Time: 27 ms (89.66%), Space: 13.3 MB (19.06%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   vector<int> findBall(vector<vector<int>> &grid)
+    {
+        vector<int> result;
+        for (int i = 0; i < grid[0].size(); i++)
+        {
+            result.push_back(helper(grid, 0, i));
+        }
+        return result;
+    }
+    int helper(vector<vector<int>> &grid, int x, int y)
+    {
+        if (x >= grid.size())
+            return y;
+        if (y >= grid[0].size())
+            return -1;
+        if (y < 0)
+            return -1;
+        if (grid[x][y] == 1 && (y == grid[0].size() - 1 || grid[x][y + 1] != 1))
+            return -1;
+        if (grid[x][y] == -1 && (y == 0 || grid[x][y - 1] != -1))
+            return -1;
+        if (grid[x][y] == 1)
+            return helper(grid, x + 1, y + 1);
+        else
+            return helper(grid, x + 1, y - 1);
+    }
+};
