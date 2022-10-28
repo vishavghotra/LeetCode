43c146ef16599869621a12467a910cba76ec4fde
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2022-10-28 18:27:19 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (84.59%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    set<int> contain;
     bool isHappy(int n)
     {
-        if (contain.find(n) != contain.end())
-            return false;
-        contain.insert(n);
+        int fast = next(n);
+        int slow = n;
+        while (fast != 1 && fast != slow)
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        return fast == 1 || fast != slow;
+    }
+    int next(int n)
+    {
         int sum = 0;
         int a = 0;
-        while (n != 0)
+        while (n > 0)
         {
             a = n % 10;
             sum += a * a;
             n = n / 10;
         }
-        if (sum == 1)
-            return true;
-        else
-        {
-            return isHappy(sum);
-        }
+        return sum;
     }
 };
