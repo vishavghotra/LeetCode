09f133d2a54f99119f63c54808fd12601eb97184
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2023-01-16 20:24:28 +0100

    Time: 18 ms (77.92%), Space: 17 MB (78.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
   vector<vector<int>> result;
-    int min_ = newInterval[0];
-    int max_ = newInterval[1];
+        
+        for (size_t i = 0; i < intervals.size(); i++)
+        {
+			//  the new interval is after the range of other interval, so we can leave the current interval baecause the new one does not overlap with it
+            if (intervals[i][1] < newInterval[0])
+            {
+                result.push_back(intervals[i]);
+            }
+			// the new interval's range is before the other, so we can add the new interval and update it to the current one
+            else if (intervals[i][0] > newInterval[1])
+            {
+                result.push_back(newInterval);
+                newInterval = intervals[i]; 
+            }
+			// the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval 
+            else // if(intervals[i][1] >= newInterval[0] || intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
 
-    int n = intervals.size();
-    int i = 0;
-    while (i < n && intervals[i][1] < min_)
-    {
-      result.push_back(intervals[i]);
-      i++;
-    }
-    while (i < n && intervals[i][0] <= max_)
-    {
-      min_ = min(intervals[i][0], min_);
-      max_ = max(intervals[i][1], max_);
-      i++;
-    }
-    result.push_back({min_, max_});
-    while (i < n)
-    {
-      result.push_back(intervals[i]);
-      i++;
-    }
-    return result;
+            }
+        }
+        
+        result.push_back(newInterval); 
+        return result;
     }
 };
