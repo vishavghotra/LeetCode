a3eb595655cf09da88e15c2da528cd9de8fb334e
vishavghotra <101067978+vishavghotra@users.noreply.github.com>
2023-01-27 00:27:37 +0100

    Time: 286 ms (5.00%), Space: 50.3 MB (5.80%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
 int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
   {
-    vector<vector<pair<int, int>>> tree(n);
+     vector<vector<pair<int, int>>> tree(n);
     for (int i = 0; i < flights.size(); i++)
     {
       tree[flights[i][0]].push_back({flights[i][1], flights[i][2]});
     }
-    queue<pair<int, int>> queue;
     vector<int> dist(n, INT_MAX);
-    queue.push({src, 0});
     dist[src] = 0;
-    while (!queue.empty() && k >= 0)
+    for (int i = 0; i <= k; i++)
     {
-      int size = queue.size();
-      for (int i = 0; i < size; i++)
+      vector<int> temp(dist);
+      for (auto flight : flights)
       {
-        auto p = queue.front();
-        queue.pop();
-        for (auto [neighbour, price] : tree[p.first])
+        if (dist[flight[0]] != INT_MAX)
         {
-          if (dist[neighbour] > price + p.second)
-          {
-            dist[neighbour] = price + p.second;
-            queue.push({neighbour, dist[neighbour]});
-          }
+          temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
         }
       }
-      k--;
+      dist = temp;
     }
     return dist[dst] == INT_MAX ? -1 : dist[dst];
 }
